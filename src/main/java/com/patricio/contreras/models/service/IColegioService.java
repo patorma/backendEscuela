de168b766a629e0c3f152b8170e4071aee6c177d
patorma <pocontreras@ing.ucsc.cl>
2020-02-14 18:41:25 -0300

    se termino el IColegioService

--- src/main/java/com/patricio/contreras/models/service/IColegioService.java
+++ src/main/java/com/patricio/contreras/models/service/IColegioService.java
@@ -7,6 +7,12 @@ import com.patricio.contreras.models.repository.Colegio;
 public interface IColegioService {
 	
 	public List<Colegio> findAll();
+	
 	public Colegio findById (Long id);
+	
+	public Colegio save(Colegio colegio);
+	
+	public void delete(Long id);
+	
 
 }
