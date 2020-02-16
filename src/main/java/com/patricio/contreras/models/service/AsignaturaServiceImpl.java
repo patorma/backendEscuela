a27b5de699fd880d125a2284f98d0df97d998b3e
patorma <pocontreras@ing.ucsc.cl>
2020-02-16 14:48:00 -0300

    implementando la interfaz de IAsignaturaService

--- src/main/java/com/patricio/contreras/models/service/AsignaturaServiceImpl.java
+++ src/main/java/com/patricio/contreras/models/service/AsignaturaServiceImpl.java
@@ -2,20 +2,31 @@ package com.patricio.contreras.models.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.patricio.contreras.models.dao.IAsignaturaDao;
 import com.patricio.contreras.models.repository.Asignatura;
 
+@Service
 public class AsignaturaServiceImpl implements IAsignaturaService {
-
+	
+	@Autowired
+	IAsignaturaDao asignaturaDao;
+	
 	@Override
+	@Transactional(readOnly = true)
 	public List<Asignatura> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return asignaturaDao.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Asignatura findById(Long id) {
 		// TODO Auto-generated method stub
-		return null;
+		return asignaturaDao.findById(id).orElse(null);
 	}
 
 	@Override
